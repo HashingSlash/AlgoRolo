9117a4f19cccfb01dc5549865bbbdf5a0a037847
HashingSlash <dcjpreece@protonmail.com>
2022-04-10 18:00:02 +0800

    AlgoRolo v1
    known app and address IDs from listed dApp Docs / sites. Python script to add easier. made to use with AlgoCSV, but may be useful.

---
+++ AlgoRolo.py
@@ -0,0 +1,58 @@
+#----------#        IMPORTS
+import requests
+import json
+import csv
+import datetime
+import base64
+
+def saveDB(DB, name):
+    DBJson = json.dumps(DB)
+    outFile = open(name + '.json', 'w')
+    outFile.write(DBJson)
+    outFile.close()
+
+def loadDB(fileName):
+    inFile = open(fileName, 'r')
+    loadFile = json.load(inFile)
+    inFile.close()
+    return loadFile
+
+
+try:
+    addressDB = loadDB('AlgoAddressDB.json')
+    appDB = loadDB('AlgoAppDB.json')
+    print('Loaded App and Address DBs')
+except:
+    addressDB = {}
+    appDB = {}
+    print('blank App and Address DBs')
+    pass
+
+print('Add new Address or dApp ID?')
+addOrApp = input('(A)ddress or dApp (I)D: ').lower()
+if addOrApp == 'a':
+    newAddress = input('Paste address to check: ').upper()
+    if newAddress not in addressDB:
+        print('Adding new address')
+        platform = input('What platform uses this address? (EG: Tinyman, Yieldly, Rand Gallery): ')
+        addressDef = input('What is this address used for? (EG: ALGO/USDC Swap, LP Token Staking, AKC/ALGO Sale: ')
+        addressDB.update({newAddress: [platform, addressDef]})
+        saveDB(addressDB, 'AlgoAddressDB')
+        print('updated addressDB')
+    else:
+        print('Address exists', addressDB[newAddress])
+elif addOrApp == 'i':
+    newApp = input('Paste app id: ')
+    if newApp not in appDB:
+        print('Adding new dApp ID')
+        platform = input('What platform uses this app ID? (EG: Tinyman, Yieldly, AlgoFi): ')
+        appDef = input('What is this app ID used for? (EG: ALGO/USDC Swap, LP Token Staking, OPUL>OPUL Staking: ')
+        appDB.update({newApp : [platform, appDef]})
+        saveDB(appDB, 'AlgoAppDB')
+        print('updated appDB')
+    else:
+        print('dApp ID exists', appDB[newApp])
+else:
+    print('input error')
+
+
