ab7e96ae4315a5a1e23b6e4b11186c6bf2a1ba69
HashingSlash <dcjpreece@protonmail.com>
2022-04-25 06:18:09 +0800

    v1.0.2
    Put script into a loop to ease adding multiple entries
    Added PACT Rewards drops, Algofund Gov 1, Algostake drop address + NFT#1 market, The algo faucet address. AlgoFi - new USDC/STBL AFns staking program LP

--- AlgoRolo.py
+++ AlgoRolo.py
@@ -28,31 +28,35 @@ except:
     print('blank App and Address DBs')
     pass
 
-print('Add new Address or dApp ID?')
-addOrApp = input('(A)ddress or dApp (I)D: ').lower()
-if addOrApp == 'a':
-    newAddress = input('Paste address to check: ').upper()
-    if newAddress not in addressDB:
-        print('Adding new address')
-        platform = input('What platform uses this address? (EG: Tinyman, Yieldly, Rand Gallery): ')
-        addressDef = input('What is this address used for? (EG: ALGO/USDC Swap, LP Token Staking, AKC/ALGO Sale: ')
-        addressDB.update({newAddress: [platform, addressDef]})
-        saveDB(addressDB, 'AlgoAddressDB')
-        print('updated addressDB')
+run = 'y'
+while run == 'y':
+    print('Add new Address or dApp ID?')
+    addOrApp = input('(A)ddress or dApp (I)D: ').lower()
+    if addOrApp == 'a':
+        newAddress = input('Paste address to check: ').upper()
+        if newAddress not in addressDB:
+            print('Adding new address')
+            platform = input('What platform uses this address? (EG: Tinyman, Yieldly, Rand Gallery): ')
+            addressDef = input('What is this address used for? (EG: ALGO/USDC Swap, LP Token Staking, AKC/ALGO Sale: ')
+            addressDB.update({newAddress: [platform, addressDef]})
+            saveDB(addressDB, 'AlgoAddressDB')
+            print('updated addressDB')
+        else:
+            print('Address exists', addressDB[newAddress])
+    elif addOrApp == 'i':
+        newApp = input('Paste app id: ')
+        if newApp not in appDB:
+            print('Adding new dApp ID')
+            platform = input('What platform uses this app ID? (EG: Tinyman, Yieldly, AlgoFi): ')
+            appDef = input('What is this app ID used for? (EG: ALGO/USDC Swap, LP Token Staking, OPUL>OPUL Staking: ')
+            appDB.update({newApp : [platform, appDef]})
+            saveDB(appDB, 'AlgoAppDB')
+            print('updated appDB')
+        else:
+            print('dApp ID exists', appDB[newApp])
     else:
-        print('Address exists', addressDB[newAddress])
-elif addOrApp == 'i':
-    newApp = input('Paste app id: ')
-    if newApp not in appDB:
-        print('Adding new dApp ID')
-        platform = input('What platform uses this app ID? (EG: Tinyman, Yieldly, AlgoFi): ')
-        appDef = input('What is this app ID used for? (EG: ALGO/USDC Swap, LP Token Staking, OPUL>OPUL Staking: ')
-        appDB.update({newApp : [platform, appDef]})
-        saveDB(appDB, 'AlgoAppDB')
-        print('updated appDB')
-    else:
-        print('dApp ID exists', appDB[newApp])
-else:
-    print('input error')
+        print('input error')
+    run = input('Run again? (y/n): ')
+    print('\n')
 
 
